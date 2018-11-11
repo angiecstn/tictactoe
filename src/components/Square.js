67af6559e238c001954da762fc28c321457c1833
Angelica Costantini <angelicacostantini07@gmail.com>
2018-11-11 16:40:24 +0100

    adding alert on click

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,7 +4,7 @@ class Square extends Component {
     state = {};
     render() {
         return (
-            <button className="square">
+            <button className="square" onClick={() => { alert('click'); }}>
                 {this.props.value}
             </button>
         );
